27eae25bcaf9b9790a1e2c1dcca566f1c139033b
PratyushSharma <psvt2007@gmail.com>
2025-08-29 15:46:47 +0530

    megablog

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  console.log(import.meta.env.VITE_APPWRITE_COLLECTION_ID)
+  return (<>
+  
+  <p>ho</p>
+  </>  )
+}
+
+export default App
