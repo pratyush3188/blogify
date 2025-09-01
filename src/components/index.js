41d3a5aed7e45c304be082a49fcb2ab20a6e4d03
PratyushSharma <psvt2007@gmail.com>
2025-09-01 22:30:18 +0530

    index file

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Select from "./post-form/Select";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+// import Logo from "./Logo";
+import LogoutBtn from "./Header/Logoutbut";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Button from "./Button";
+import PostForm from "./post-form/Postform";
+import PostCard from "./Postcard";
+import AuthLayout from "./Authlayout";
+import Input from "./Input";
+export {
+    Header,
+    Footer,
+    Container,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    Button,
+    PostForm,
+    PostCard,
+    AuthLayout, 
+    Input,
+    Select,
+}
