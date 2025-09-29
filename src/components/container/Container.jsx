b0c7e5f21ade6dc12d739ffdb63b9137c17a4c4d
PratyushSharma <psvt2007@gmail.com>
2025-09-29 22:39:50 +0530

    container

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-
-function Container({children}) {
-  return <div className='w-full max-w-7xl mx-auto px-4'>{children}</div>;
-  
+function Container({ children }) {
+  return (
+    <div className="w-full px-4 sm:px-6 lg:px-8 overflow-x-hidden">
+      <div className="max-w-screen-2xl mx-auto">
+        {children}
+      </div>
+    </div>
+  );
 }
-
 export default Container
