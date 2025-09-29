da09480deb068e558e9e2ffda79ec4bd052b8021
PratyushSharma <psvt2007@gmail.com>
2025-09-29 22:43:13 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import './App.css'
+import React, { useState, useEffect } from "react"
+import { useDispatch } from "react-redux"
+import "./App.css"
 import authService from "./appwrite/auth"
-import {login, logout} from "./store/authSlice"
-import { Footer, Header } from './components'
-import { Outlet } from 'react-router-dom'
+import { login, logout } from "./store/authSlice"
+import { Footer, Header } from "./components"
+import { Outlet } from "react-router-dom"
 
 function App() {
   const [loading, setLoading] = useState(true)
@@ -12,26 +12,25 @@ function App() {
 
   useEffect(() => {
     authService.getCurrentUser()
-    .then((userData) => {
-      if (userData) {
-        dispatch(login({userData}))
-      } else {
-        dispatch(logout())
-      }
-    })
-    .finally(() => setLoading(false))
-  }, [])
-  
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [dispatch])
+
   return !loading ? (
-    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
-      <div className='w-full block'>
-        <Header />
-        <main>
-          <Outlet />
-        </main>
-        <Footer />
-      </div>
-    </div>
+    <div className="min-h-screen flex flex-col bg-gray-200 text-black overflow-x-hidden">
+  <Header />
+  <main className="flex-1">
+    <Outlet />
+  </main>
+  <Footer />
+</div>
+
   ) : null
 }
 
